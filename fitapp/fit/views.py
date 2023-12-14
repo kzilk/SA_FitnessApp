961b669bcf91f562615718c29f970e0a7289f545
kzilk <78563339+kzilk@users.noreply.github.com>
2023-12-14 16:34:05 -0600

    id removal and validation update
    no longer prints id columns from SQL tables, no longer states required fields on sections not in use

--- fitapp/fit/views.py
+++ fitapp/fit/views.py
@@ -31,71 +31,80 @@ class Index(View):
    
     def post(self, request):
         bfResult = None
-
-        form = bfCalcForm(request.POST)
-        if form.is_valid():
-            # process bfCalc data and perform calculations
-            bfHeight = int(form.cleaned_data['height'])
-            bfWaist = int(form.cleaned_data['waist'])
-            bfNeck = int(form.cleaned_data['neck'])
-            bfHip = int(form.cleaned_data['hips'])
-
-            logH = math.log10(bfHeight)
-
-            # 163.205×log10(waist+hip-neck) - 97.684×(log10(height)) - 78.387
-
-            if bfHip > 0:
-                logWHN = math.log10(bfWaist + bfHip - bfNeck)
-                result = 163.205 * logWHN - 97.684 * (logH) - 78.387
-                bfResult = round(result, 2)
-            else:
-                logWN = math.log10(bfWaist - bfNeck)
-                result = 86.010 * logWN - 70.041 * logH + 36.76
-                bfResult = round(result, 2)
-
-            print("Form valid")
-        else:
-            print("Form is not valid")
-            print(form.errors)
-
-        # handle update weight progress
-        progress_form = progressForm(request.POST)
-        if progress_form.is_valid():
-            print('Progress Valid')
-            progress_form.save()  # save to database
-            return redirect(request.path) #refresh page to view updated table
-        else:
-            print('Progress invalid')
-            print(progress_form.errors)
-
-        # handle update lift progress
-        lift_form = liftForm(request.POST)
-        if lift_form.is_valid():
-            print('lift valid')
-            lift_form.save()
-            return redirect(request.path) #refresh page to view updated table
-        else:
-            print('Lift invalid')
-            print(lift_form.errors)
-
-        # handle routine selection
-        routineRec = routineForm(request.POST)
+        form = bfCalcForm()
+        progress_form = progressForm()
+        lift_form = liftForm()
+        routineRec = routineForm()
         routine_selection = None
-        if routineRec.is_valid():
-            routine_selection = routineRec.cleaned_data['routine_choice']
-        else:
-            print('Routine invalid')
-            print(routineRec.errors)
-            
-
-        # handle targeting muscle group
-        targetRec = targetForm(request.POST)
+        targetRec = targetForm()
         target_selection = None
-        if targetRec.is_valid():
-            target_selection = targetRec.cleaned_data['target_choice']
-        else:
-            print('Target invalid')
-            print(targetRec.errors)
+
+        if 'bfButton' in request.POST:
+            form = bfCalcForm(request.POST)
+            if form.is_valid():
+                # process bfCalc data and perform calculations
+                bfHeight = int(form.cleaned_data['height'])
+                bfWaist = int(form.cleaned_data['waist'])
+                bfNeck = int(form.cleaned_data['neck'])
+                bfHip = int(form.cleaned_data['hips'])
+
+                logH = math.log10(bfHeight)
+
+                # 163.205×log10(waist+hip-neck) - 97.684×(log10(height)) - 78.387
+
+                if bfHip > 0:
+                    logWHN = math.log10(bfWaist + bfHip - bfNeck)
+                    result = 163.205 * logWHN - 97.684 * (logH) - 78.387
+                    bfResult = round(result, 2)
+                else:
+                    logWN = math.log10(bfWaist - bfNeck)
+                    result = 86.010 * logWN - 70.041 * logH + 36.76
+                    bfResult = round(result, 2)
+
+                print("Form valid")
+            else:
+                print("Form is not valid")
+                print(form.errors)
+
+        elif 'weightButton' in request.POST:
+            # handle update weight progress
+            progress_form = progressForm(request.POST)
+            if progress_form.is_valid():
+                print('Progress Valid')
+                progress_form.save()  # save to database
+                return redirect(request.path) #refresh page to view updated table
+            else:
+                print('Progress invalid')
+                print(progress_form.errors)
+
+        elif 'liftButton' in request.POST:
+            # handle update lift progress
+            lift_form = liftForm(request.POST)
+            if lift_form.is_valid():
+                print('lift valid')
+                lift_form.save()
+                return redirect(request.path) #refresh page to view updated table
+            else:
+                print('Lift invalid')
+                print(lift_form.errors)
+
+        elif 'routineButton' in request.POST:
+            # handle routine selection
+            routineRec = routineForm(request.POST)
+            if routineRec.is_valid():
+                routine_selection = routineRec.cleaned_data['routine_choice']
+            else:
+                print('Routine invalid')
+                print(routineRec.errors)
+            
+        elif 'targetButton' in request.POST:
+            # handle targeting muscle group
+            targetRec = targetForm(request.POST)
+            if targetRec.is_valid():
+                target_selection = targetRec.cleaned_data['target_choice']
+            else:
+                print('Target invalid')
+                print(targetRec.errors)
 
         weight_data = progressModel.objects.all()
         lift_data = liftModel.objects.all()
